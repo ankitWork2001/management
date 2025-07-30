36cb6e6199a5eb8c13f90ee3d7d985b636da2267
KapilSharma1306 <kapilsharmacoding1306202@gmail.com>
2025-07-30 13:05:59 +0530

    create employee profile page

--- src/pages/Employee/ProfileView.jsx
+++ src/pages/Employee/ProfileView.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import profile_image from '../../assets/profile_photo.png'
+import { FaEdit } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
+import { ImCross } from "react-icons/im";
 
 function ProfileView() {
+  const [edit,setEdit] = useState(false)
+  const [data,setData] = useState({
+    profile_photo:profile_image,
+    name:'Hitesh Odedara',
+    email:'ohitesh60@gmail.com',
+    phone:'9265178967',
+    address:'365, Malubu Point',
+    position:'Manager',
+    employee_type:'Full Time'
+  })
+
+  const getData = (e) => {
+     const name = e.target.name;
+     const value = e.target.value;
+     setData(pre => ({
+      ...pre,
+      [name]:value
+     }))
+  }
+
+  const saveData = (e) => {
+     e.preventDefault()
+     setEdit(false)
+  }
   return (
-    <div>
-      Profile
+    <div className='flex justify-center items-center md:my-10 mt-15'>
+      <div className='flex flex-col gap-5 items-center'>
+        <h2 className='text-2xl'>Personal Info</h2>
+        <div className='bg-black px-10 py-10 rounded-3xl '>
+          <div className='flex gap-7'>
+            <img src={data.profile_photo} alt="image" />
+            <div className='space-y-3'>
+              <div className='flex justify-between'>
+                <label htmlFor="name" className='text-xl'>Name</label>
+                {edit ? <ImCross onClick={() => setEdit(false)} className='cursor-pointer text-[#0075FF] '/> :<FaEdit onClick={() => setEdit(true)} className='text-xl text-[#0075FF] rounded-lg cursor-pointer'/>}
+              </div>
+              <input type="text" readOnly={!edit} className="p-1 bg-gray-800 text-[#A0AEC0]  read-only:outline-gray-500 rounded pl-4 read-only:outline-1 read-only:cursor-not-allowed read-only:rounded"  id='name' name='name' value={data.name} onChange={getData}/>
+            </div>
+          </div>
+          <form onSubmit={saveData} className='mt-5 space-y-4'>
+            <div className='flex flex-col gap-2'>
+              <label htmlFor="email" className='text-xl'>Email</label>
+              <input type="text" name='email' value={data.email} onChange={getData} readOnly={!edit} className="p-1 bg-gray-800 rounded pl-4 text-[#A0AEC0]  read-only:outline-gray-500 read-only:outline-1 read-only:rounded read-only:cursor-not-allowed" />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <label htmlFor="phone" className='text-xl'>Phone</label>
+              <input type="number" name='phone' value={data.phone} onChange={getData} readOnly={!edit} className="p-1 bg-gray-800 rounded pl-4 text-[#A0AEC0]  read-only:outline-gray-500 read-only:outline-1 read-only:rounded read-only:cursor-not-allowed" />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <label htmlFor="address" className='text-xl'>Address</label>
+              <input type="text" name='address' value={data.address} onChange={getData} readOnly={!edit} className="p-1 bg-gray-800 rounded pl-4 text-[#A0AEC0]  read-only:outline-gray-500 read-only:outline-1 read-only:rounded read-only:cursor-not-allowed" />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <label htmlFor="position" className='text-xl'>Position</label>
+              <input type="text" name='position' value={data.position} onChange={getData} readOnly={!edit} className="p-1 bg-gray-800 rounded pl-4 text-[#A0AEC0]  read-only:outline-gray-500 read-only:outline-1 read-only:rounded read-only:cursor-not-allowed" />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <label htmlFor="employee_type" className='text-xl'>Employee Type</label>
+              <input type="text" name='employee_type' value={data.employee_type} onChange={getData}  readOnly={!edit} className="p-1 bg-gray-800 rounded pl-4 text-[#A0AEC0]  read-only:outline-gray-500 read-only:outline-1 read-only:rounded read-only:cursor-not-allowed" />
+            </div>
+            <button className={`${edit ? 'block' : 'hidden'} outline-none cursor-pointer bg-[#0075FF] hover:bg-blue-600 transition-bg duration-300 w-full text-xl py-2 rounded mt-3`}>Save</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
